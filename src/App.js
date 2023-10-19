85a9e71112defd793e0c425cc6c901840998d843
Manshish Agrawal <manshishagrawal131999@gmail.com>
2023-10-20 02:20:03 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import {Routes,Route,Link} from "react-router-dom";
+import Login from './components/login';
+import Register from './components/register';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">UNIO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Retals</Nav.Link>
+            <Nav.Link href="#pricing">Vehicles</Nav.Link>
+            
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets"> <Link to={"/login"}><Button variant="outline-secondary">Login</Button></Link></Nav.Link>
+            <Nav.Link href="#memes"> <Link to={"/register"}><Button variant="info">Register</Button></Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <div className="container-fluid mt-1">
+       <Routes>
+         <Route path="/" element={<Login />}></Route>
+         <Route path="/login" element={<Login />}></Route>
+         <Route path="/register" element={<Register />}></Route>
+       </Routes>
+
+    </div>
+     
     </div>
   );
 }
---
+++ src/components/register.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Register() {
+  return (
+    <div>shivi is not best she is best in world</div>
+  )
+}
